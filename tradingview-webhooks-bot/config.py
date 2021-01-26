36e5cfd9a92fd3d578b2d6820e8bcf56df133973
L8nit3tr0ubl3 <l8nit3@outlook.com>
2021-01-25 19:13:50 -0500

    Remove unused setting

--- tradingview-webhooks-bot/config.py
+++ tradingview-webhooks-bot/config.py
@@ -1,6 +1,5 @@
 API = "" #BotFather API Key
 ID = "-100"  #Name of Channel
-UseMultipleAccounts = False
 Accounts = { 1: {"Name": "BOT_NAME", "EXCHANGEAPI": '*PLACE_API_KEY_HERE', "EXCHANGESECRET": 'PLACE_API_SECRET_HERE'},
             #2: {"Name": "BOT_NAME", "EXCHANGEAPI": '*PLACE_API_KEY_HERE', "EXCHANGESECRET": 'PLACE_API_SECRET_HERE'}
             }
