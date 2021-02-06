d75fe58dc637d1732d8392b58fa7d9199facc4d4
L8nit3tr0ubl3 <l8nit3@outlook.com>
2021-02-06 13:27:10 -0500

    Update config.py

--- tradingview-webhooks-bot/config.py
+++ tradingview-webhooks-bot/config.py
@@ -1,5 +1,6 @@
 API = "" #BotFather API Key
 ID = "-100"  #Name of Channel
+UseTelegram = True
 Accounts = { 1: {"Name": "BOT_NAME", "EXCHANGEAPI": '*PLACE_API_KEY_HERE', "EXCHANGESECRET": 'PLACE_API_SECRET_HERE'},
             #2: {"Name": "BOT_NAME", "EXCHANGEAPI": '*PLACE_API_KEY_HERE', "EXCHANGESECRET": 'PLACE_API_SECRET_HERE'}
             }
