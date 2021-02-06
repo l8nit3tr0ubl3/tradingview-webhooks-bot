a7dd86877ca09fd6eb7ec36458f27b2425a08abc
L8nit3tr0ubl3 <l8nit3@outlook.com>
2021-02-06 15:27:48 -0500

    Separate alerts for multiple accounts (FTX  and TGRAM)

--- tradingview-webhooks-bot/config.py
+++ tradingview-webhooks-bot/config.py
@@ -1,6 +1,7 @@
-API = "" #BotFather API Key
-ID = "-100"  #Name of Channel
 UseTelegram = True
+TelegramAccounts = { 1: {"Name": "Main Bot", "API": '#BotFather APi Key#', "ID": '-100 #channel ID#'},
+                     #2: {"Name": "Majors", "API": '#BotFather Api Key#', "ID": '-100#Channel ID#'}
+                   }
 Accounts = { 1: {"Name": "BOT_NAME", "EXCHANGEAPI": '*PLACE_API_KEY_HERE', "EXCHANGESECRET": 'PLACE_API_SECRET_HERE'},
             #2: {"Name": "BOT_NAME", "EXCHANGEAPI": '*PLACE_API_KEY_HERE', "EXCHANGESECRET": 'PLACE_API_SECRET_HERE'}
             }
