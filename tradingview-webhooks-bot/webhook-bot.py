695a82c4e5ee7cc7e58253cc3b96c2d5cb8f884e
L8nit3tr0ubl3 <l8nit3@outlook.com>
2021-02-22 22:06:41 -0500

    Many bugfixes

--- tradingview-webhooks-bot/webhook-bot.py
+++ tradingview-webhooks-bot/webhook-bot.py
@@ -1,66 +1,66 @@
 """
 Tradingview-webhooks-bot is a python bot that works with tradingview's webhook alerts!
-This bot is not affiliated with tradingview and was created by @robswc
-
-You can follow development on github at: github.com/robswc/tradingview-webhook-bot
-
-I'll include as much documentation here and on the repo's wiki!  I
-expect to update this as much as possible to add features as they become available!
-Until then, if you run into any bugs let me know!
+This bot is not affiliated with tradingview and was originally created by @robswc.
+It has been modified for PERSONAL USE by l8nit3.
 """
-
-from actions import send_order, parse_webhook, SendToTelegram
+from actions import send_order, parse_webhook, send_to_telegram, post_tweet
 from auth import get_token
 from flask import Flask, request, abort
-import config as config
+import config
 import ccxt
 
 # Create Flask object called app.
 app = Flask(__name__)
 
-
-# Create root to easily let us know its on/working.
 @app.route('/')
 def root():
+    """# Create root to easily let us know its on/working."""
     return 'online'
-
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
+    """Combine all functions in actions.py, and run flask server"""
     if request.method == 'POST':
         print('*'*60)
         # Parse the string data from tradingview into a python dict
         data = parse_webhook(request.get_data(as_text=True))[0]
         tail = parse_webhook(request.get_data(as_text=True))[1]
-        if data['type'] != "Skip":
+        message = tail
+        if (data['type'] != "Skip") and (get_token() == data['key']):
             # Check that the key is correct
-            if get_token() == data['key']:
-                print(' [Alert Received] ')
-                print('POST Received:', data)
-                for account, AccountInfo in config.Accounts.items():
-                    if AccountInfo['Name'] in data['BotName']:
-                        print("BotInfo Name {0} MATCHES TelegramName {1}".format(AccountInfo['Name'], data['BotName']))
-                        exchange = ccxt.ftx({'apiKey': AccountInfo['EXCHANGEAPI'], 'secret': AccountInfo['EXCHANGESECRET'], 'enableRateLimit': True,})
-                        send_order(data, tail, exchange, AccountInfo['Name'])
-                    else:
-                        print("BotInfo Name {0} not match TelegramName {1}".format(AccountInfo['Name'], data['BotName']))
-                        pass
-                if config.UseTelegram == True:
-                    SendToTelegram(tail, data)
-                return '', 200
-            else:
-                #pass
-                abort(403)
+            print(' [Alert Received] ')
+            print('POST Received:', data)
+            for account, account_info in config.Accounts.items():
+                if account_info['Name'] in data['BotName']:
+                    exchange = ccxt.ftx({'apiKey': account_info['exchangeapi'],
+                    'secret': account_info['exchangesecret'],
+                    'enableRateLimit': True,})
+                    if account_info['Subaccount'] == 'Yes':
+                        exchange.headers = {'FTX-SUBACCOUNT': account_info['SubName'],}
+                    wins, losses, accuracy = send_order(data, tail, exchange, account_info['Name'], account_info['Track'])
+                    if account_info['UseTelegram'] == 'Yes':
+                        message = send_to_telegram(tail, data)
+                    if account_info['UseTwitter'] == 'Yes':
+                        post_tweet(message)
+                else:
+                    pass
+            return '', 200
+
         if data['type'] == "Skip":
             print(' [ALERT ONLY - NO TRADE] ')
             print('POST Received:', data)
             print(tail)
-            SendToTelegram(tail, data)
-            return '', 200
+            for account, account_info in config.Accounts.items():
+                if account_info['UseTelegram'] == 'Yes':
+                    message = send_to_telegram(tail, data)
+                if account_info['UseTwitter'] == 'Yes':
+                    post_tweet(message)
+        print('*'*60, '/n')
+        return '', 200
     else:
         abort(400)
-    print('*'*60, '/n')
 
 
 if __name__ == '__main__':
+    """Run app"""
     app.run()
